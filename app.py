a8efa682927115a3ccd9d85177462a264278734a
srikanth <kokkirapatisrikanth347@gmail.com>
2026-03-15 17:11:03 +0530

    add gunicorn

--- app.py
+++ app.py
@@ -528,8 +528,9 @@ def authorize_google():
     return flask.redirect(flask.url_for("index"))
 
 
+
 import os
 
-
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
